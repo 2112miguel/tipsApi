05b802c34ada30e7c6d2f05e86d7d97b48fa487f
2112miguel <miguel.moreno.angel@gmail.com>
2022-05-03 10:24:53 -0500

    [Add] Se agregaron los componentes initalValue

---
+++ src/Components/initalValue/InitalValue.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './InitalValue.scss';
+
+const InitalValue = () => {
+  return (
+    <div>
+      <input className="InitalValue" type="text" value="0"></input>
+    </div>
+  );
+};
+
+export default InitalValue;
