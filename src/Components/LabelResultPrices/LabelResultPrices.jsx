b4e6cc75b9681732fb12bae1c034d5ef0b299dbd
2112miguel <miguel.moreno.angel@gmail.com>
2022-05-03 12:07:42 -0500

    [Add] Se agrego el componente labelResultPrices

---
+++ src/Components/LabelResultPrices/LabelResultPrices.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import './LabelResultPrices.scss';
+const LabelResultPrices = () => {
+  return <div className="LabelResultPrices">$ 0.00</div>;
+};
+
+export default LabelResultPrices;
