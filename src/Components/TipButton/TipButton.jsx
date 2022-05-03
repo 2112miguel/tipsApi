f99dfd5ba4648460c3da8639385962bbf3df3e50
2112miguel <miguel.moreno.angel@gmail.com>
2022-05-03 11:20:02 -0500

    [Add] Se agrego componentes de TipButton a Tips

---
+++ src/Components/TipButton/TipButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TipButton.scss';
+
+const TipButton = () => {
+  return (
+    <div>
+      <button className="TipButton">%</button>
+    </div>
+  );
+};
+
+export default TipButton;
