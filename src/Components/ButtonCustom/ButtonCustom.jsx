0c3477f3941024c49c5e617ee1ff173002e46a51
2112miguel <miguel.moreno.angel@gmail.com>
2022-05-03 19:35:43 -0500

    [Add] Se agrego componente ButtonCustom

---
+++ src/Components/ButtonCustom/ButtonCustom.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ButtonCustom.scss';
+
+const ButtonCustom = () => {
+  return (
+    <div className="">
+      <button className="ButtonCustom">Custom</button>
+    </div>
+  );
+};
+
+export default ButtonCustom;
